0f16b7bc1b38057be0893e2c3cde2ef9d9708640
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2016-12-14 00:47:56 +0530

    added the header file (*.h)

---
+++ daivat.h
@@ -0,0 +1,8 @@
+void project       (void);
+void typecheck     (void);
+void rangecheck    (void);
+void formatcheck   (void);
+void lengthcheck   (void);
+void conditional   (void);
+void divisonproblem (void);
+
